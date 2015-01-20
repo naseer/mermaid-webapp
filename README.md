f7e70fa77709f6ba873fd02a87227d1364e23a3b
Naseer Ahmed <naseer.ahmed@gmail.com>
2015-01-19 22:38:18 -0500

    Add: Angular webapp for mermaid

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# mermaid-webapp
-Simple angular webapp allowing for creation of graphs and sequence diagrams on the web Uses [mermaid](ihttps://github.com/knsv/mermaid) and [angularjs](https://angularjs.org/)
+mermaid-webapp
+===============
+
+Simple angular webapp allowing for creation of graphs and sequence diagrams on the web
+Uses [mermaid](https://github.com/knsv/mermaid) and [angularjs](https://angularjs.org/)
+
+
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,39 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularMermaidApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the angularMermaidApp
+ */
+angular.module('angularMermaidApp')
+  .controller('MainCtrl', ['$scope', '$sce', function($scope, $sce) {
+    $scope.checkUpdate = function() {
+      setTimeout(function() {
+        console.log('scope updated:' + $scope.mermaidsyntax);
+        var mermaidholder = document.getElementById('mermaidholder');
+        //Delete the exisiting child nodes
+        while (mermaidholder.firstChild) {
+          mermaidholder.removeChild(mermaidholder.firstChild);
+        }
+
+        //Add the new node
+        var mermaidnode = document.createElement('div');
+        mermaidnode.className = 'mermaid';
+        mermaidnode.appendChild(document.createTextNode($sce.trustAsHtml($scope.mermaidsyntax)));
+        mermaidholder.appendChild(mermaidnode);
+
+        mermaid.init(); // jshint ignore:line
+      }, 2000);
+    };
+
+    $scope.mermaidsyntax = 'sequenceDiagram\n' +
+      'A->> B: Query\n' +
+      'B->> C: Forward query\n' +
+      'Note right of C: Thinking...\n' +
+      'C->> B: Response\n' +
+      'B->> A: Forward response\n';
+    $scope.checkUpdate();
+
+  }]);
