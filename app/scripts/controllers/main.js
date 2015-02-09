5df8dd1fc8d516c9bc26616965cf11f6470e9df9
Naseer Ahmed <naseer.ahmed@gmail.com>
2015-02-09 12:46:38 -0500

    Use mermaid parse to show errors

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -10,23 +10,32 @@
 angular.module('angularMermaidApp')
   .controller('MainCtrl', ['$scope', '$sce', '$location', function($scope, $sce, $location) {
     $scope.absurl = '';
-    $scope.diaglink='';
+    $scope.diaglink = '';
+    $scope.showerror = false;
+
     $scope.checkUpdate = function() {
-      $scope.diaglink=$scope.absurl+'##'+encodeURIComponent($scope.mermaidsyntax);
+      $scope.diaglink = $scope.absurl + '##' + encodeURIComponent($scope.mermaidsyntax);
       setTimeout(function() {
+        var syntax = $sce.trustAsHtml($scope.mermaidsyntax) + '\n';
+        // Delete and re add the mermaid node from the DOM
         var mermaidholder = document.getElementById('mermaidholder');
         //Delete the exisiting child nodes
         while (mermaidholder.firstChild) {
           mermaidholder.removeChild(mermaidholder.firstChild);
         }
 
-        //Add the new node
-        var mermaidnode = document.createElement('div');
-        mermaidnode.className = 'mermaid';
-        mermaidnode.appendChild(document.createTextNode($sce.trustAsHtml($scope.mermaidsyntax)));
-        mermaidholder.appendChild(mermaidnode);
-        console.log($scope.diaglink);
-        mermaid.init(); // jshint ignore:line
+        if (mermaid.parse(syntax)) { //jshint ignore:line
+          $scope.showerror = false;
+          //Add the new node
+          var mermaidnode = document.createElement('div');
+          mermaidnode.className = 'mermaid';
+          mermaidnode.appendChild(document.createTextNode($sce.trustAsHtml($scope.mermaidsyntax)));
+          mermaidholder.appendChild(mermaidnode);
+          console.log($scope.diaglink);
+          mermaid.init(); // jshint ignore:line
+        } else {
+          $scope.showerror = true;
+        }
       }, 1000);
     };
 
@@ -36,13 +45,13 @@ angular.module('angularMermaidApp')
       //Delete the other elements and leave only the diagram
       $scope.showform = false;
       $scope.diagclass = 'col s12 m12 l12';
-      $scope.cardclass='';
+      $scope.cardclass = '';
 
     } else {
       $scope.absurl = $location.absUrl();
       $scope.showform = true;
       $scope.diagclass = 'col s12 m12 l9';
-      $scope.cardclass='card';
+      $scope.cardclass = 'card';
       $scope.mermaidsyntax = 'sequenceDiagram\n' +
         'A->> B: Query\n' +
         'B->> C: Forward query\n' +
